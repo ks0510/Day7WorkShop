935a8c0bc034c835172c806fc90960e434b38129
ks0510 <kshaikh051999@gmail.com>
2023-04-23 08:44:25 +0530

    Added java program to initialize satke and bet amount for gambling game

---
+++ StartGambleGame.java
@@ -0,0 +1,27 @@
+package gambler;
+
+/**
+ * 
+ * @author Kaif
+ *
+ */
+
+public class StartGambleGame {
+
+	/**
+	 * This is the starting point of program and main function
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		/*
+		 * To start game with 100$ toatal amount and 1$ at stake
+		 */
+
+		int totalAmount = 100;
+
+		int betAmount = 1;
+	}
+}
